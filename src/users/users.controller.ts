71941595646d91d1b362fa2cfa9be6728bb3e5af
Michael Trew <mtrew2015@gmail.com>
2023-10-29 15:12:54 -0400

    adding exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +10,28 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto){
     const { email, password } = body;
     this.usersService.create(email, password);
+  }
+  @Get('/users/:id')
+  async findUser(@Param('id') id: string){
+    const user = await this.usersService.findOne(parseInt(id));
+    if(!user){
+      throw new NotFoundException('user not found');
+    }
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get('/users')
+  findAllUsers(@Query('email') email: string){
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string){
+    return this.usersService.remove(parseInt(id));
   }
 }
