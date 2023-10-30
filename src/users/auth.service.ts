980ccb5c47174467ae6a50b9e10be95e45604bdd
Michael Trew <mtrew2015@gmail.com>
2023-10-30 10:21:09 -0400

    working on auth route for signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService) {
+  }
+  async signUp(email: string, password: string){
+    // see if email is in use
+    const users = await this.usersService.find(email);
+    // hash users password
+    if(users.length){
+      throw new BadRequestException('email in use');
+    }
+
+    // create a new user and save it
+
+    // return the user
+  }
+
+  signIn(){}
+
 }
