ef035608860ded1ebbb47a021eac1d11113ab32b
Michael Trew <mtrew2015@gmail.com>
2023-11-05 14:28:26 -0500

    sign up and cookie test e2e

--- test/auth-e2e-spec.ts
+++ test/auth-e2e-spec.ts
@@ -27,4 +27,19 @@ describe('Auth System', () => {
         expect(email).toEqual(email);
   });
 });
+
+it('signup as a new user then get the currently logged in user', async () => {
+  const email = 'test@gmail.com';
+  const res = await request(app.getHttpServer())
+    .post('/auth/signup')
+    .send({ email, password: '123456' })
+    .expect(201);
+  const cookie = res.get('Set-Cookie');
+  const { body } = await request(app.getHttpServer())
+    .get('/auth/whoami')
+    .set('Cookie', cookie)
+    .expect(200);
+  expect(body.email).toEqual(email);
+});
+
 });
