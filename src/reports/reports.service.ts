b475af6c6d4d51493b70dea62c0658ddcf981ac3
Michael Trew <mtrew2015@gmail.com>
2023-11-05 15:38:47 -0500

    working on report service and routes

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create(createReportDto: CreateReportDto) {
+    return this.repo.save(createReportDto);
+  }
+
+  findAll() {
+    return `This action returns all reports`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} report`;
+  }
+
+  update(id: number, updateReportDto: any) {
+    return `This action updates a #${id} report`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} report`;
+  }
+}
